f91d133b42b2d7775c70d1e7636c4b76cfc18067
Brian Kelly <polymonic@gmail.com>
2018-01-31 20:35:00 -0500

    Only show recordings as Recent Activity on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.all.order(created_at: :desc)
+    @activities = PublicActivity::Activity.all.where(trackable_type: 'Recording').order(created_at: :desc)
   end
 end
